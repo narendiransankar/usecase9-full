75eafa6b60601eece8dd181fd6f049f63762d963
narendiransankar <narendiransankar1699@gmail.com>
2025-07-25 00:16:18 +0530

    code updated

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -5,7 +5,9 @@ resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [var.oidc_thumbprint]
-
+  lifecycle {
+    prevent_destroy = true
+  }
   tags = {
     Name = "GitHub-Actions-OIDC"
   }
